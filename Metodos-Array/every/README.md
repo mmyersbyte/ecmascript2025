02cee74ead0b520d10ed88865cc485e2cfc5237a
mmyersbyte <pedrovictorcastrodesousa@gmail.com>
2025-08-01 23:38:27 -0300

    feat: add every example

---
+++ Metodos-Array/every/README.md
@@ -0,0 +1,5 @@
+O método every em JavaScript é uma função de array que testa se todos os elementos do array passam por uma condição especificada em uma função callback. Ele retorna true se todos os elementos atenderem à condição; caso contrário, retorna false.
+
+Exemplos no código
+
+Refêrencia: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/every#sintaxe
--- Metodos-Array/every/index.js
+++ Metodos-Array/every/index.js
@@ -1,5 +1,11 @@
 const idades = [15, 32, 33, 16, 40];
 
-const result = idades.every((idade) => idade > 8);
+const resultTrue = idades.every((idade) => idade > 8); // Verifica se todas as idades são maiores que 8c
 
-console.log(result);
+console.log(resultTrue);
+// true, pois todas as idades são maiores que 8
+
+const resultFalse = idades.every((idade) => idade > 18); // Verifica se todas as idades são maiores que 18
+
+console.log(resultFalse);
+// false, pois nem todas as idades são maiores que 18
